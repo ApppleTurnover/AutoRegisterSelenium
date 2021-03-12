22480ce909a0556c572bc80224fba6a09e84af17
ApppleTurnover <apppleturnover@gmail.com>
2021-03-13 07:34:39 +1000

    I did a bit of refactoring. Realized creation of several accounts with randomized data.

--- create_account.py
+++ create_account.py
@@ -29,34 +29,36 @@ def is_email_valid(email) -> bool:
     return bool(re.match(pattern, email))
 
 
-def get_domains():
+def get_domains() -> list:
+    """Return domains list"""
     url = "https://privatix-temp-mail-v1.p.rapidapi.com/request/domains/"
 
     response = requests.request("GET", url, headers=headers)
+    if not isinstance(response.json(), list):
+        raise Exception("Domain acquisition error")
     return response.json()
 
 
 def get_mails(email: str):
+    """Return mails list from random mail"""
     url = os.environ.get('url_for_mail') + hashlib.md5(email.encode()).hexdigest() + '/'
 
     response = requests.request("GET", url, headers=headers)
     return response.json()
 
 
-def save_login_and_password(email: str, login=None, password=None):
+def parse_and_save_data(email: str, login=None, password=None):
     messages = get_mails(email)
-    for i in range(12):
+    for i in range(5):
         try:
             if messages.get('error'):
-                print(messages.get('error'))
-                print(f'The mail did not arrive. It will be repeated in 5 seconds ({i + 1}/12)')
+                print(f'The mail did not arrive. It will be repeated in 5 seconds ({i + 1}/5)')
                 time.sleep(5)
                 messages = get_mails(email)
         except AttributeError:
             break
-
     else:
-        raise Exception('The mail did not arrive')
+        print('The mail did not arrive on' + email)
 
     for message in messages:
         if 'успешно создан' in message['mail_subject']:
@@ -66,12 +68,17 @@ def save_login_and_password(email: str, login=None, password=None):
 
     if login and password:
         with open('accounts_data.txt', 'a+') as f:
-            f.write(f"[\ne:{email};\nl:{login};\np:{password}\n],\n")
+            f.write(f"[\ne:{email};\nl:{login};\np:{password}\n],")
 
 
 def check_data(name, email):
-    if not is_email_valid(email) or not len(name) > 3:
+    if not is_email_valid(email) or len(name) < 3:
         raise Exception("Invalid data")
+
+
+def randomed_data():
+    """Return list with random full name(index 0) email(index 1)"""
+    return random_string(20), (random_string(20) + random.choice(domains)).lower()
 
 
 def input_missing(func):
@@ -80,49 +87,52 @@ def input_missing(func):
     def inner():
         arguments = func()
         questions = [
-            inquirer.List('browser',
-                          message='What browser do you want to use?',
-                          choices=browsers),
+            inquirer.List('browser', message='What browser do you want to use?', choices=browsers),
             inquirer.Text('name', message='Full name: '),
             inquirer.Text('email', message='E-Mail: ')
         ]
 
-        if arguments.get('browser') is None:
-            browser = inquirer.prompt([questions[0], ]).get('browser')
+        browser = arguments.get('browser') if arguments.get('browser') \
+            else inquirer.prompt([questions[0], ]).get('browser')
+
+        if arguments.get('rand'):
+            return {
+                'browser': browser,
+                'rand': arguments.get('rand')
+            }
+
         else:
-            browser = arguments.get('browser')
+            name, email = arguments.get('name'), arguments.get('email')
 
-        if not arguments.get('rd'):
-            if arguments.get('name') is None:
-                name = inquirer.prompt([questions[1], ]).get('name')
-            else:
-                name = arguments.get('name')
-
-            if arguments.get('email') is None:
-                email = inquirer.prompt([questions[2], ]).get('email')
-            else:
-                email = arguments.get('email')
-
+            # TODO: Refactor
+            if not arguments.get('name'):
+                name = inquirer.prompt((questions[1],)).get('name')
+            if not arguments.get('email'):
+                email = inquirer.prompt((questions[2],)).get('email')
             check_data(name, email)
 
-        else:
-            name = random_string(20)
-            email = (random_string(20) + random.choice(get_domains())).lower()
-
-        return {
-            'browser': browser,
-            'name': name,
-            'email': email,
-            'rd': arguments.get('rd'),
-        }
+            return {
+                'browser': browser,
+                'name': name,
+                'email': email,
+                'rand': arguments.get('rand'),
+            }
 
     return inner
 
 
-def register(browser, name, email, rd):
-    driver = webdriver.__dict__[browser]()
-    driver.implicitly_wait(10)
-    try:
+def register(browser, name, email, count):
+    """TimeWeb account registration"""
+
+    is_random = bool(count)
+    if not is_random:
+        count = 1
+
+    for i in range(count):
+        driver = webdriver.__dict__[browser]()
+        driver.implicitly_wait(10)
+        if is_random:
+            name, email = randomed_data()
         driver.get('https://timeweb.com/ru/services/hosting/')
         driver.find_element_by_xpath("//a[text() = 'Начать тест']").click()
 
@@ -133,40 +143,65 @@ def register(browser, name, email, rd):
         field_email.send_keys(email)
 
         driver.find_element_by_xpath("//div[text() = 'Стать клиентом']").click()
-    finally:
-        time.sleep(5)
+        time.sleep(1)
+        parse_and_save_data(email)
+        print(f"Complete \\({i + 1})")
         driver.quit()
-        if rd:
-            save_login_and_password(email)
-
 
 @input_missing
 def get_attr() -> dict:
     parser = argparse.ArgumentParser()
     account_settings = parser.add_argument_group('Account settings')
 
-    # TODO: Refactor(remove duplicate code)
-    account_settings.add_argument('--browser', '-b', default=None, help='Browser You Want to Use')
-    account_settings.add_argument('--name', '-n', default=None, help='Full Name')
-    account_settings.add_argument('--email', '-e', default=None, help='E-Mail Address')
-    account_settings.add_argument('--random-data', '-r', default=False,
-                                  help='Use Random Full Name and E-Mail or No. Attention! temp-mail.org mail will be updated',
-                                  action=argparse.BooleanOptionalAction)
+    arguments = {
+        'browser': {
+            'options': ['--browser', '-b'],
+            'type': str,
+            'default': None,
+            'help': "Browser You Want to Use"
+        },
+        'name': {
+            'options': ['--name', '-n'],
+            'type': str,
+            'default': None,
+            'help': "Full Name"
+        },
+        'email': {
+            'options': ['--email', '-e'],
+            'type': str,
+            'default': None,
+            'help': "E-Mail Address"
+        },
+        'random_data': {
+            'options': ['--random-data', '-r'],
+            'type': int,
+            'default': 0,
+            'help': "number of accounts with random names and posts. (The entered name and mail will be ignored)",
+        }
+    }
+    for argument in arguments.values():
+        account_settings.add_argument(
+            argument['options'][0], argument['options'][1],
+            default=argument.get('default'),
+            type=argument.get('type'),
+            help=argument.get('help'),
+        )
 
     arguments = parser.parse_args()
     return {
         'browser': arguments.browser,
         'name': arguments.name,
         'email': arguments.email,
-        'rd': arguments.random_data,
+        'rand': arguments.random_data,
     }
 
 
 if __name__ == '__main__':
+    domains = get_domains()
     args = get_attr()
     register(
         browser=args.get('browser'),
         name=args.get('name'),
         email=args.get('email'),
-        rd=args.get('rd')
+        count=args.get('rand'),
     )
